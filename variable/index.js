46210ae09d1f75bdf22e4de2bd9973cbfbd10473
Taurouf <tauroufouf@gmail.com>
2020-09-23 11:33:33 +0200

    feat(feat): Add exercice 3

--- variable/index.js
+++ variable/index.js
@@ -22,3 +22,21 @@
 *My Digital School 
 *forever every day
 */
+
+/**
+* Exercice 3
+*/
+
+var string = "42";
+var number = 42;
+var float = 42.00;
+var boolean = false;
+var array = [];
+var date = new Date();
+var objet = {
+    chiffre : 42
+};
+var test2;
+var test = null;
+
+console.log(string, number, float, boolean,array,date,objet,test2,test)
