bbb2b2a7084ba4afd57992aa4148c8414fa27afc
Taurouf <tauroufouf@gmail.com>
2020-09-23 11:32:51 +0200

    feat(feat): Add exercice 2

--- variable/index.js
+++ variable/index.js
@@ -5,3 +5,20 @@
 */
 
 //My Digital School Forever
+
+'use strict';
+
+/**
+* Exercice 1
+*/
+
+//My Digital School Forever
+
+/**
+* Exercice 2
+*/
+
+/**
+*My Digital School 
+*forever every day
+*/
